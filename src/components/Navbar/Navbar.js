2648d092a4e97f865a977c91be2dce9bd396840a
Carlos Murillo <carlos-murillo-o@outlook.com>
2022-08-24 16:58:54 -0500

    functional menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './Navbar.css';
 import Header from '../Header/header';
 import NavigationLinks from '../NavigationLinks/NavigationLinks';
-import NavigationData from '../../api/servicio.json';
 
-import data from '../../api/servicio.json';
+import Axios from 'axios';
+
 
 
 function Navbar (){
     
-    console.log(NavigationData.lista[0].pays_transfer)
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+      Axios.get('/data/servicio.json')
+      .then((response) => setData(response.data.lista))
+      .catch(() => setError(true));
+  }, []);
+
+  console.log(setData)
 
   return (
     <div className='navbarContainer'>
           <Header/>
-          <NavigationLinks NavigationData={NavigationData}/>
+          <div>
+            {
+              data.length > 0 ? <NavigationLinks data={data} /> : 
+              <nav className='menu'>
+                  <ul>
+                      <li>{error ? 'Ha ocurrido un error inesperado.' : 'Cargando ...'}</li>
+                  </ul>
+              </nav>                    
+            }
+          </div>
     </div>
   )
 }
